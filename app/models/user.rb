a280f072c3ee36ff958e8605f960151b20219ce7
Mostafa <mostafa@analytics4life.com>
2014-05-27 00:27:24 -0400

    Reject emails with two consecuitive dots and check for lower-casing before save

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,11 @@
 class User < ActiveRecord::Base
-	
-	before_save { self.email = email.downcase }
+
+	# change the email to lower-case
+	before_save { email.downcase! }
 
 	validates :name, presence: true, length: { maximum: 50 }
 
-	VALID_EMAIL_REGX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+	VALID_EMAIL_REGX = /\A[\w+\-.]+@[a-z\d\-]+(?:\.[a-z\d\-]+)*\.[a-z]+\z/i 
 	validates :email, presence: true, 
 					  format: { with: VALID_EMAIL_REGX },
 					  uniqueness: { case_sensitive: false }
